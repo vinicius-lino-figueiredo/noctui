6332ce6bf40cf91993f658365da7bb451bb883ee
Vinícius Lino Figueirêdo <viniciusfigueiredo725@protonmail.com>
2025-03-13 14:31:41 -0300

    ✨ feat: Adding workspace control
    Adding workspace commands (list, get)

--- go.mod
+++ go.mod
@@ -3,11 +3,14 @@ module main
 go 1.24.0
 
 require (
+	github.com/olekukonko/tablewriter v0.0.5
 	github.com/spf13/cobra v1.9.1
 	github.com/vinicius-lino-figueiredo/insomnium v0.0.0
 )
 
 require (
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/mattn/go-runewidth v0.0.16 // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
 	github.com/spf13/pflag v1.0.6 // indirect
 )
---
+++ workspace.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
+
+	"github.com/spf13/cobra"
+	"github.com/vinicius-lino-figueiredo/insomnium"
+)
+
+func init() {
+	var quiet, noPager, asJson, asJsonList *bool
+	var cols *[]string
+	WsCmd := &cobra.Command{
+		Use:     "workspace",
+		Short:   "Workspace management",
+		Args:    cobra.NoArgs,
+		Aliases: []string{"ws", "wrk"},
+		Version: "v0.0.0",
+	}
+
+	WsLsCmd := &cobra.Command{
+		Use:     "list",
+		Short:   "List all workspaces",
+		Args:    cobra.NoArgs,
+		Aliases: []string{"ls"},
+		Version: "v0.0.0",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			paginate := !(*noPager)
+			buf := bytes.NewBuffer(nil)
+			if *asJsonList {
+				_ = json.NewEncoder(buf).Encode(inso.Workspaces)
+				return Output(buf, paginate)
+			}
+			if *asJson {
+				for _, ws := range inso.Workspaces {
+					_ = json.NewEncoder(buf).Encode(ws)
+				}
+				return Output(buf, paginate)
+			}
+			if *quiet {
+				for _, ws := range inso.Workspaces {
+					buf.WriteString(ws.ID + "\n")
+				}
+				return Output(buf, paginate)
+			}
+			t := Table{}
+			for _, ws := range inso.Workspaces {
+				r := map[string]string{
+					"ID":       ws.ID,
+					"Name":     ws.Name,
+					"Modified": fmt.Sprint(ws.Modified),
+					"ParentID": ws.ParentID,
+				}
+				t = append(t, r)
+			}
+			err := t.Render(buf, *cols...)
+			if err != nil {
+				return err
+			}
+			return Output(buf, paginate)
+		},
+	}
+
+	WsGetCmd := &cobra.Command{
+		Use:     "get",
+		Args:    cobra.ExactArgs(1),
+		Short:   "Get workspace info",
+		Long:    "Get basic json info about the given workspace",
+		Version: "v0.0.0",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			paginate := !(*noPager)
+			id := args[0]
+			var wrk *insomnium.Workspace
+			for _, ws := range inso.Workspaces {
+				if ws.ID == id {
+					wrk = &ws
+					break
+				}
+			}
+			if wrk == nil {
+				return errors.New("workspace not found")
+			}
+			buf := bytes.NewBuffer(nil)
+			err := json.NewEncoder(buf).Encode(wrk)
+			if err != nil {
+				return err
+			}
+			Output(buf, paginate)
+			return nil
+		},
+	}
+
+	WsCmd.AddCommand(WsLsCmd, WsGetCmd)
+
+	quiet = WsCmd.PersistentFlags().BoolP("quiet", "q", false, "Return id only")
+	noPager = WsCmd.PersistentFlags().BoolP("no-pager", "P", false, "Disable pager")
+	asJson = WsLsCmd.PersistentFlags().BoolP("as-json", "j", false, "As json")
+	asJsonList = WsLsCmd.PersistentFlags().BoolP("as-json-list", "J", false, "As json")
+	cols = WsLsCmd.PersistentFlags().StringSliceP("cols", "c", []string{"ID", "Name", "Modified", "ParentID"}, "As json")
+
+	rootCmd.AddCommand(WsCmd)
+}
